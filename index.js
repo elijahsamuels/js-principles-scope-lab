ab6391be24dd41bfc01f2ed28eaf13237064cae9
Elijah Samuels <elijahsamuels@gmail.com>
2021-02-18 17:22:20 -0700

    commit tree 8dea0144ed055f1a1f29d5a2416c06e1fc9911e6

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+// declare customerName to be bob in global scope
+//       ✓ returns the customerName
+
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+	// console.log(customerName.toUpperCase());
+	customerName = customerName.toUpperCase();
+};
+
+function setBestCustomer() {
+	bestCustomer = "not bob"
+};
+
+function overwriteBestCustomer() {
+	bestCustomer = "maybe bob"
+};
+
+const leastFavoriteCustomer = "Jim";
+
+function changeLeastFavoriteCustomer() {
+	leastFavoriteCustomer = "not jim or bob"
+};
